13e59558ba441659e27a734625261c9e1f82d4e9
CelesteSMThomas <celestesmthomas@gmail.com>
2021-04-15 09:26:42 -0700

    meals controller, index and create route

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -1,4 +1,22 @@
 class Api::V1::MealsController < ApplicationController
 
-    
+    def index
+        @meals = Meal.all
+        render json: @meals 
+    end
+
+    def create
+        meal = Meal.new(meal_params)
+        if meal.save
+            render json: meal, status: :accepted
+        else
+            render json: {errors: meal.errors.full_messages}, status: :unprocessible_entity 
+        end
+    end
+
+private
+    def meal_params
+        params.require(:meal).permit(:name, :image_url, :calories, :ingridents, :category_id)
+    end
+
 end
